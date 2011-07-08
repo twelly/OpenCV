4c57687a8574c4562cc7264cd12b15e08c436619
Wilson Talaugon <twelly@gmail.com>
2011-07-08 00:55:56 -0700

    Added comments

--- Exercises/binarization/binarization.c
+++ Exercises/binarization/binarization.c
@@ -11,10 +11,13 @@ int main()
     IplImage* imgOriginal = cvLoadImage("superlotto.tif", 1);
     IplImage* imgResult = cvCreateImage(cvSize(imgOriginal->width, imgOriginal->height), IPL_DEPTH_8U, 1);
     cvZero(imgResult);
+    /* convert to grayscale */
     cvCvtColor(imgOriginal,imgResult,CV_RGB2GRAY);
     imgGray = cvCloneImage(imgResult);
+    /* apply gaussian blur */
     cvSmooth(imgGray, imgResult, CV_GAUSSIAN, 1, 1);
     imgSmooth = cvCloneImage(imgResult);
+    /* apply inverse binarization */
     cvThreshold(imgSmooth, imgResult,32,255, CV_THRESH_BINARY_INV);
     imgThresh = cvCloneImage(imgResult);
     cvDilate(imgThresh,imgResult,NULL,2);
